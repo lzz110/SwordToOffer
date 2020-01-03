ab7f04d65b20076e4136afdf38ea64999627473f
lzz110 <46778165+lzz110@users.noreply.github.com>
2020-01-03 14:57:49 +0800

    add note

--- src/TreeNodeClass/SerializeAndDeserialize.java
+++ src/TreeNodeClass/SerializeAndDeserialize.java
@@ -2,7 +2,11 @@ package TreeNodeClass;
 
 /**
  * 题目：请实现两个函数，分别用来序列化和反序列化二叉树
- *
+ * 例如：	    1
+ *			2		3
+ *		4		 5     6
+ * 结果：1,2,4,#,#,#,3,5,#,#,6,#,#,(前序遍历)
+ *  
  * 思路：
  *      序列化  ：遍历二叉树，遇到叶子节点，将其转化为 $ 表示。
  *      反序列化：根据前序遍历的特点（根、左、右），进行二叉树的还原
@@ -37,7 +41,8 @@ public class SerializeAndDeserialize {
         sb.append(Serialize(root.right));
         return sb.toString();
     }
-    TreeNode Deserialize(String str) {
+    
+	TreeNode Deserialize(String str) {
         index++;
         int len=str.length();
         TreeNode node=null;
@@ -46,7 +51,7 @@ public class SerializeAndDeserialize {
         }
         String[] splitstr = str.split(",");
         if(!splitstr[index].equals("#")){
-            node=new TreeNode(Integer.valueOf(splitstr[index]));
+            node=new TreeNode(Integer.parseInt(splitstr[index]));	//将String[] 的单个字符转换成int 存入TreeNode中
             node.left=Deserialize(str);
             node.right=Deserialize(str);
         }
